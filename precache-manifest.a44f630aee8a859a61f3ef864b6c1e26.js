916a7466f162f05b87c85ea9b21635b6f126e85f
charlie wynn <github@cwynn.com>
2019-12-12 17:39:47 -0600

    Updates

---
+++ precache-manifest.a44f630aee8a859a61f3ef864b6c1e26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f20260874c861cf26931802024c43b2",
+    "url": "/mema-football-2020/index.html"
+  },
+  {
+    "revision": "964441d0e7c70585d165",
+    "url": "/mema-football-2020/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0d20ed2e408cb697290c",
+    "url": "/mema-football-2020/static/js/2.bc770990.chunk.js"
+  },
+  {
+    "revision": "eaa93899759cd78ff2a4147c0af30aa2",
+    "url": "/mema-football-2020/static/js/2.bc770990.chunk.js.LICENSE"
+  },
+  {
+    "revision": "964441d0e7c70585d165",
+    "url": "/mema-football-2020/static/js/main.2dc3037b.chunk.js"
+  },
+  {
+    "revision": "fa925bfc7efd8995a0d1",
+    "url": "/mema-football-2020/static/js/runtime-main.623fd219.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mema-football-2020/static/media/logo.5d5d9eef.svg"
+  }
+]);
