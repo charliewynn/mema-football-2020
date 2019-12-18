a6e4c51eb493d1ae5588d8e51247f2b2f90d86e4
charlie wynn <github@cwynn.com>
2019-12-18 16:17:19 -0600

    adds logout and shows username

--- src/Components/Login.tsx
+++ src/Components/Login.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import * as firebase from "firebase/app";
 import "firebase/auth";
@@ -10,17 +10,34 @@ type LoginProps = {
 };
 
 export default ({ title, paragraph }: LoginProps) => {
+  const [signedIn, setSignedIn] = useState(false);
+  const [user, setUser] = useState("");
+  firebase.auth().onAuthStateChanged(user => {
+    console.log("User", user);
+    setUser(user?.displayName || "");
+    setSignedIn(!!user);
+  });
   const uiConfig = {
     // Popup signin flow rather than redirect flow.
     signInFlow: "popup",
     // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-    signInSuccessUrl: "/signedIn",
+    //signInSuccessUrl: "/signedIn",
     // We will display Google and Facebook as auth providers.
     signInOptions: [firebase.auth.EmailAuthProvider.PROVIDER_ID]
   };
   return (
     <div id="firebase_auth">
-      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+      {signedIn ? (
+        <div>
+          hey {user}
+          <button onClick={() => firebase.auth().signOut()}>Sign-out</button>
+        </div>
+      ) : (
+        <StyledFirebaseAuth
+          uiConfig={uiConfig}
+          firebaseAuth={firebase.auth()}
+        />
+      )}
     </div>
   );
 };
